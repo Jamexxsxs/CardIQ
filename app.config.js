5de98dcb200289b3242b1f8c3b85c3726ae7d85c
Jomary Coquilla <jomarycoquilla465@gmail.com>
2025-05-30 12:41:51 +0800

    prod

--- app.config.js
+++ app.config.js
@@ -3,19 +3,34 @@ import fs from 'fs';
 
 export default ({ config }) => {
     const iconPath = path.resolve(__dirname, './assets/app-icon.png');
-
     const iconExists = fs.existsSync(iconPath);
+
     return {
         ...config,
         name: 'CardIQ',
-        slug: 'card-iq',
+        slug: 'CardIQ', 
+        version: '1.0.0', 
         android: {
-        ...config.android,
-        icon: iconExists ? './assets/app-icon.png' : config.android?.icon,
+            ...config.android,
+            icon: iconExists ? './assets/app-icon.png' : config.android?.icon,
+            versionCode: 1 
+        },
+        ios: {
+            ...config.ios,
+            icon: iconExists ? './assets/app-icon.png' : config.ios?.icon,
+            buildNumber: '1', 
         },
         extra: {
-        API_KEY: process.env.API_KEY,
-        PDFCO_API_KEY: process.env.PDFCO_API_KEY
+            ...config.extra,
+            API_KEY: process.env.API_KEY,
+            PDFCO_API_KEY: process.env.PDFCO_API_KEY,
+            eas: {
+                projectId: "3b53a795-8b5f-4258-bbbb-f1aa875514ae"
+            }
+        },
+        cli: {
+            ...config.cli,
+            appVersionSource: 'local'
         }
     };
 };
